c5fd106c666c6265eb320a7aa5b860f8377f0aa4
Leticia Pinheiro <leticiapinheiro3137@gmail.com>
2022-03-26 19:06:47 -0300

    princesa

---
+++ Princesa.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+// usando o vector
+bool existe( vector<int>& fila, int n)
+{
+    for( int i = 0; i < fila.size(); i++ ){
+        if( fila[i] == n ) 
+        return true;
+    }
+    return false;
+}
+
+int main()
+{ 
+    bool flag = existe();
+
+}
