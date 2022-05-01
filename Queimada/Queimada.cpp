684204272e7a6e96693953356662e81c8b93898d
Leticia Pinheiro <leticiapinheiro3137@gmail.com>
2022-05-01 12:17:35 -0300

    queimada

---
+++ Queimada/Queimada.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void mostrar(vector<string> map)
+{
+    for (auto line : map){
+        cout << line << endl;
+    }
+    getchar();
+
+}
+
+void queimar(vector<string> map, int lpos, int cpos)
+{
+   
+    int nl =  map.size();
+    int nc = map[0].size();
+
+    if(lpos < 0 || lpos >= nl || cpos < 0 || cpos >= nc){
+        return;
+    }
+    
+    if(map[lpos][cpos] == '#'){
+        map[lpos][cpos] = 'x'; // pegou fogo
+        mostrar(map);
+    }
+
+    queimar(map, lpos, cpos - 1);
+    queimar(map, lpos - 1, cpos);
+    queimar(map, lpos, cpos + 1);
+    queimar(map, lpos + 1, cpos);
+    
+}
+
+int main() 
+{
+    vector<string> map;
+    ifstream arq("txt.queimada.txt");
+    int nl { }, nc { }, li{ }, ci{ };
+    arq >> nl >> nc >> li >> ci;
+
+    string line;
+    for(int i = 0; i < nl; i++){
+        arq >> line;
+        map.push_back(line);
+    }
+    
+    queimar(map, li, ci);
+    mostrar(map);
+}
